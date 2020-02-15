cf61c767ec54869e06305a6ddad40ed2076c892b
hoang <giahoangth@gmail.com>
2020-02-15 10:43:49 +0700

    correct tailwind config

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     fontFamily: {
       "sans": 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
       "serif": 'Georgia, Cambria, "Times New Roman", Times, serif',
-      "lacquer": 'Lacquer", serif'
+      "lacquer": 'Lacquer, serif'
     }
   },
   variants: {},
