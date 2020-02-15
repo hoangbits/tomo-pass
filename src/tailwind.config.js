9ad84cc0abd69ec87008385a14a366df642ee3c1
hoang <giahoangth@gmail.com>
2020-02-15 09:16:46 +0700

    import lacquer font-face

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
-    extend: {}
+    fontFamily: {
+      "sans": 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      "serif": 'Georgia, Cambria, "Times New Roman", Times, serif',
+      "lacquer": 'Lacquer", serif'
+    }
   },
   variants: {},
   plugins: []
