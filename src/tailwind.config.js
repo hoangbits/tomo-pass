5f52c7e33ac9c22a2b6291824c16f3a15a9d093d
hoang <giahoangth@gmail.com>
2020-02-16 16:15:03 +0700

    generate config fonts

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,29 @@
+const _ = require("lodash");
 module.exports = {
   theme: {
     fontFamily: {
       "sans": 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
       "serif": 'Georgia, Cambria, "Times New Roman", Times, serif',
       "lacquer": 'Lacquer, serif'
+    },
+    fontSize: {
+       'xs': '.75rem',
+       'sm': '.875rem',
+       'tiny': '.875rem',
+       'base': '1rem',
+       'lg': '1.125rem',
+       'xl': '1.25rem',
+       '2xl': '1.5rem',
+       '3xl': '1.875rem',
+       '4xl': '2.25rem',
+       ..._.reduce(
+        new Array(6),
+        (accumulator, current, index) => {
+          accumulator[`${index + 5}xl`] = `${index + 3}rem`;
+          return accumulator
+        },
+        {}
+      )
     }
   },
   variants: {},
