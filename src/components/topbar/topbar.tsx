ce4354154d8df6a3d72802969795ccb9e3eca68a
hoang <giahoangth@gmail.com>
2020-03-23 14:56:48 +0700

    config gh-pages

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-
+import React from "react";
 
 const Topbar: React.FC = () => {
-	return (
-		<div className="">
-			<h1 className="font-lacquer">TOMOPASS</h1>
-		</div>
-	)
+  return (
+    <div className="">
+      <h1 className="text-red-800 font-lacquer text-8xl``">TOMOPASS</h1>
+    </div>
+  );
 };
 
 export default Topbar;
