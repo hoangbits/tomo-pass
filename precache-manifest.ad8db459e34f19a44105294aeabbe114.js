0af4d621ec20065c8a52dd2c622a9681b242297e
hoang <giahoangth@gmail.com>
2020-03-23 08:45:30 +0700

    Updates

---
+++ precache-manifest.ad8db459e34f19a44105294aeabbe114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d07dcb666bddfa5aef0acbb1a3322ea",
+    "url": "/tomo-pass/index.html"
+  },
+  {
+    "revision": "e5f0c25cc50f4f4a5e15",
+    "url": "/tomo-pass/static/css/main.6ead6546.chunk.css"
+  },
+  {
+    "revision": "cd1a0dfcc021f4a8e81a",
+    "url": "/tomo-pass/static/js/2.b4de60e9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tomo-pass/static/js/2.b4de60e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5f0c25cc50f4f4a5e15",
+    "url": "/tomo-pass/static/js/main.36162f3a.chunk.js"
+  },
+  {
+    "revision": "85ff2202822841733654",
+    "url": "/tomo-pass/static/js/runtime-main.5b4e288a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55e56de5b7184bb15c6f0e8147ce7dec.js"
+  "/tomo-pass/precache-manifest.ad8db459e34f19a44105294aeabbe114.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tomo-pass/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
