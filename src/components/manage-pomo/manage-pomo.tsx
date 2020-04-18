425d722a821a7710c4dfcffb6c7756af9a75c507
hoang <giahoangth@gmail.com>
2020-04-18 11:24:06 +0700

    set event handler

--- src/components/manage-pomo/manage-pomo.tsx
+++ src/components/manage-pomo/manage-pomo.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import { AgGridReact } from "@ag-grid-community/react";
-import { AgGridEvent } from "@ag-grid-community/core/dist/cjs/events";
+import {
+  AgGridEvent,
+  CellEditingStoppedEvent
+} from "@ag-grid-community/core/dist/cjs/events";
 import { GridApi } from "@ag-grid-community/core/dist/cjs/gridApi";
 import { ColumnApi } from "@ag-grid-community/core/dist/cjs/columnController/columnApi";
 import { AllCommunityModules } from "@ag-grid-community/all-modules";
 import "@ag-grid-community/all-modules/dist/styles/ag-grid.css";
 import "@ag-grid-community/all-modules/dist/styles/ag-theme-alpine.css";
 import styled from "styled-components";
-import tw from 'tailwind.macro';
+import tw from "tailwind.macro";
+import { NewValueParams } from "@ag-grid-community/core/dist/es6/entities/colDef";
 
 interface ManagePomoProps {}
 
 const StyledGrid = styled(AgGridReact)`
-   ${tw`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
+  ${tw`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
 `;
 
 const ManagePomo: React.FC<ManagePomoProps> = () => {
@@ -39,10 +43,24 @@ const ManagePomo: React.FC<ManagePomoProps> = () => {
         "2234 Sleepy Pony Mall , Drain, DC, 20078-4243, US, (202) 948-3634",
       mood: "Happy",
       country: "Ireland"
-    }
+    },
+    {},
+    {},
+    {}
   ]);
   const [columnDefs, setColumnDefs] = useState([
-    { field: "firstName" },
+    {
+      field: "firstName",
+      onCellValueChanged: function(params: NewValueParams) {
+        console.log("onCellValueChanged params", params);
+        console.log("onCellValueChanged columnDefs", rowData);
+        if (!params.data.c) {
+          params.data.c = {};
+        }
+        params.data.c.x = params.newValue;
+        return true;
+      }
+    },
     { field: "lastName" },
     { field: "gender" },
     { field: "age" },
@@ -87,12 +105,27 @@ const ManagePomo: React.FC<ManagePomoProps> = () => {
 
   const onGridReady = (params: AgGridEvent) => {
     setGridApi(params.api);
-    console.log("params", params);
     setGridColumnApi(params.columnApi);
+    console.log("params", params);
   };
 
+  const [gridOptions, setGridOptions] = useState({
+    columnDefs: columnDefs,
+    rowData: rowData,
+    defaultColDef: defaultColDef,
+    // pinnedTopRowData: getPinnedTopData(),
+    // pinnedBottomRowData: getPinnedBottomData(),
+    onGridReady: onGridReady,
+    onCellEditingStopped: function(event: CellEditingStoppedEvent) {
+      console.log("cellEditingStopped", event);
+      /**
+       * save all rows data to localStorage
+       */
+    }
+  });
+
   return (
-    <div style={{ width: "100%", height: "400px" }}>
+    <div style={{ width: "100%", height: "320px" }}>
       <div style={{ height: "calc(100% - 25px)" }}>
         <div
           id="myGrid"
@@ -102,15 +135,7 @@ const ManagePomo: React.FC<ManagePomoProps> = () => {
           }}
           className="ag-theme-alpine"
         >
-          <StyledGrid
-            modules={AllCommunityModules}
-            rowData={rowData}
-            columnDefs={columnDefs}
-            defaultColDef={defaultColDef}
-            pinnedTopRowData={getPinnedTopData()}
-            pinnedBottomRowData={getPinnedBottomData()}
-            onGridReady={onGridReady}
-          />
+          <StyledGrid modules={AllCommunityModules} gridOptions={gridOptions} />
         </div>
       </div>
     </div>
